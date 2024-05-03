7c6390f03497de7b7c9077b29eb3ddffeb55dad2
Jaredmasek <jaredmasek@yahoo.com>
2024-05-02 20:55:09 -0700

    Explanation of analysis

--- mt.cars.R
+++ mt.cars.R
@@ -30,3 +30,11 @@ reg3=lm(mpg~wt+qsec+hp+vs,data=mtcars)
 summary(reg3)
 vif(reg3)
 mtcars %>% select(wt,qsec,hp,vs) %>% cor()
+
+#Looking at the mtcars data set within R, a linear regression for mpg is found using the leaps method.
+#It initially finds a 3 variable regression using weight, 1/4 mile time, and engine type.
+#Upon further inspection, there is collinearity between 1/4 mile time and engine type, meaning both are not necessary.
+#1/4 mile time has a higher correlation with weight, so it was the variable taken out of the model.
+#Finding a regression with only weight and 1/4 mile time leads to a model with very little collinearity, and a higher R-squared adjusted
+#Value than the previous model. To further test models, the best 4-variable model was tested but similar issues arose with collinearity.
+#Therefore the 2-variable model seems best, since in practical terms would require less resources while also having better predicting accuracy.
